68271fe94fa3845d35d91cd1e7c24ee288f9ffbc
sebastian-huamani <huamanitassara@gmail.com>
2023-12-17 10:59:00 -0500

    Feat/Perfil

--- src/main/java/com/example/demo/controller/UsuariosController.java
+++ src/main/java/com/example/demo/controller/UsuariosController.java
@@ -7,6 +7,8 @@ import com.example.demo.model.response.RespuestaResponse;
 import com.example.demo.model.response.UsuarioResponse;
 import com.example.demo.model.service.UsuarioService;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -18,12 +20,20 @@ import java.util.Optional;
 @RequestMapping("/usuario")
 public class UsuariosController {
     private UsuarioService usuarioService;
+
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("usuarios", usuarioService.listarUsuarios());
         return "/usuario/index";
     }
 
+    @GetMapping("/perfil")
+    public String verPerfil(Model model){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+//        Usuario usuario =  usuarioService.findUserByUserNombres(String.valueOf(auth.getName()));
+        model.addAttribute("usuario", usuarioService.findUserByUserNombres(String.valueOf(auth.getName())));
+        return "/usuario/miPerfil";
+    }
 
     @GetMapping("/editar/{id}")
     public String editar(@PathVariable int id, Model model) {
@@ -40,6 +50,7 @@ public class UsuariosController {
         model.addAttribute("usuario", usuario);
         return "/usuario/editar";
     }
+
 
 
 }
